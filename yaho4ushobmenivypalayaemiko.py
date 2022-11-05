a577f218ae813710a9abd0587d6b276592f90317
luuklike <dergalodmitro@gmail.com>
2022-11-05 11:31:38 +0200

    Update

---
+++ yaho4ushobmenivypalayaemiko.py
@@ -0,0 +1,16 @@
+ls = [4, 2, 6, 8, 1, 9, 12]
+#for i in ls:
+    #print(i, end='')
+
+#print()
+
+#for i in range(len(ls)):
+    #print(ls[i], end=' ')
+
+# print(*ls)
+
+#for i in range(2, 4):
+    #print(ls[i])
+#ls[start:end:step]
+#print(type(ls[2:5]))
+print(ls[2::2])
