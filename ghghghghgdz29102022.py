d1ae4bbfbf131313bb9eff459944db2fed06b908
luuklike <dergalodmitro@gmail.com>
2022-11-06 20:25:26 +0200

    update

--- ghghghghgdz29102022.py
+++ ghghghghgdz29102022.py
@@ -1,4 +1,9 @@
-string = str(input("Введіть текст: "))
-exist = str(string.lower())
-part1, part2 = exist.split('description')
-print("description:", part2)
+while True:
+    try:
+        string = str(input("Введіть текст: "))
+        exist = str(string.lower())
+        part1, part2 = exist.split('description')
+        print("description:", part2)
+        break
+    except:
+        print("Будь ласка, напишіть descrition перед початком опису")
